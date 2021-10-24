4d06ee021f77cab85f68ab2b6c66aab431156c55
Ian Zimmerman <itz@no-use.mooo.com>
2021-10-24 11:32:16 -0700

    Add formula for dds

---
+++ Formula/dds.rb
@@ -0,0 +1,23 @@
+class Dds < Formula
+  desc "Double dummy bridge hand solving library"
+  homepage "https://privat.bahnhof.se/wb758135/bridge/index.html"
+  url "https://github.com/dds-bridge/dds/archive/refs/tags/v2.9.0.tar.gz"
+  sha256 "9ef36d8c36bf697ba3b499fcb9dca51a4b423278ac72e947235ac86f0b5fc38a"
+  license "Apache-2.0"
+
+  def install
+    "src/Makefiles/Makefile_Mac_clang_static".cp "src/Makefile"
+    inreplace "src/Makefile" do |s|
+      s.gsub!(/^THREADING[[:blank:]]=.*/, "THREADING = -DDDS_THREADS_STL")
+      s.gsub!(/^THREAD_LINK[[:blank:]]=.*/, "THREAD_LINK =")
+      s.gsub!(/^CC[[:blank:]]=.*/, "CC = c++")
+    end
+    system "make", "-C", "src"
+    lib.install "src/libdds.a" => "libdds.a"
+    include.install "include/dll.h" => "dds.h"
+  end
+
+  test do
+    system "false"
+  end
+end
