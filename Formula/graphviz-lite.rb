ad4961aaa9790f2c8926295cc3228234483ac073
Ian Zimmerman <itz@no-use.mooo.com>
2022-05-23 21:48:53 -0700

    ARGH! graphviz must now be built from a git repo!

--- Formula/graphviz-lite.rb
+++ Formula/graphviz-lite.rb
@@ -1,19 +1,24 @@
 class GraphvizLite < Formula
   desc "Graph visualization software from AT&T and Bell Labs"
   homepage "https://www.graphviz.org/"
-  sha256 "59931082a3638139e06c296b96e860a9d338432af06f7f57a6ad8da5cbf465c7"
-  url "https://gitlab.com/graphviz/graphviz/-/archive/3.0.0/graphviz-3.0.0.tar.gz"
-
+  url "https://gitlab.com/graphviz/graphviz.git",
+      tag:      "3.0.0",
+      revision: "24cf7232bb8728823466e0ef536862013893e567"
   license "EPL-1.0"
   version_scheme 1
+  head "https://gitlab.com/graphviz/graphviz.git", branch: "main"
 
   depends_on "pkg-config" => :build
   depends_on "bison" => :build
+  depends_on "automake" => :build
+  depends_on "autoconf" => :build
   depends_on "expat"
   depends_on "freetype"
   depends_on "fontconfig"
   depends_on "gd"
   conflicts_with "graphviz", because: "graphviz-lite is just a light build of the same package"
+
+  uses_from_macos "flex" => :build
 
   def install
     args = %W[
