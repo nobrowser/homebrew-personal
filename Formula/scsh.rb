f99b2a395fb1b38577863aa523fdf682aeca3245
Ian Zimmerman <itz@no-use.mooo.com>
2022-06-10 23:26:28 -0700

    Try adding scsh

---
+++ Formula/scsh.rb
@@ -0,0 +1,18 @@
+class Scsh < Formula
+  desc "A shell embedded in Scheme"
+  homepage "https://scsh.net"
+  url "https://github.com/scheme/scsh.git", revision: "4acf6e4ed7b65b46186ef0c9c2a1e10bef8dc052"
+  license: BSD-3-Clause
+
+  depends_on "automake" => :build
+  depends on "autoconf" => :build
+  depends_on "scheme48"
+
+  def install
+    system "autoreconf", "-i"
+    system "./configure", *std_configure_args
+    system "make"
+    system "make", "install"
+  end
+
+end
