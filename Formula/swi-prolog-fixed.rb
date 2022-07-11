582a05b7b04bfbd2ac7ff4e6f5792a5b0fc8ea29
Ian Zimmerman <itz@no-use.mooo.com>
2022-07-11 15:26:18 -0700

    Fix paths to keg-only formulas ARGGGH

--- Formula/swi-prolog-fixed.rb
+++ Formula/swi-prolog-fixed.rb
@@ -6,7 +6,7 @@ class SwiPrologFixed < Formula
   url "https://www.swi-prolog.org/download/stable/src/swipl-8.4.3.tar.gz"
   sha256 "946119a0b5f5c8f410ea21fbf6281e917e61ef35ac0aabbdd24e787470d06faa"
   license "BSD-2-Clause"
-  revision 2
+  revision 3
   head "https://github.com/SWI-Prolog/swipl-devel.git", branch: "master"
 
   livecheck do
@@ -34,6 +34,9 @@ class SwiPrologFixed < Formula
   conflicts_with "swi-prolog", because: "this is just a fixed version of swi-prolog"
 
   def install
+    ENV["PKG_CONFIG_PATH"] = "/usr/local/opt/libarchive/lib/pkgconfig:/usr/local/opt/openssl@1.1/lib/pkgconfig:/usr/local/opt/readline/lib/pkgconfig"
+    ENV["LDFLAGS"] = "-L/usr/local/opt/berkeley-db/lib -L/usr/local/opt/libarchive/lib -L/usr/local/opt/openssl@1.1/lib -L/usr/local/opt/readline/lib"
+    ENV["CPPFLAGS"] = "-I/usr/local/opt/berkeley-db/include -I/usr/local/opt/libarchive/include -I/usr/local/opt/openssl@1.1/include -I/usr/local/opt/readline/include"
     args = ["-DSWIPL_PACKAGES_JAVA=OFF", "-DCMAKE_INSTALL_RPATH=@loader_path"]
     system "cmake", "-G", "Ninja", "-S", ".", "-B", "build", *std_cmake_args, *args
     system "cmake", "--build", "build"
