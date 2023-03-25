a1f1a263e6a28cd04414bf2f92eb84e4d5faac54
Ian Zimmerman <itz@no-use.mooo.com>
2023-03-24 21:28:00 -0700

    Need model for test

--- Formula/whisper.rb
+++ Formula/whisper.rb
@@ -5,10 +5,16 @@ class Whisper < Formula
   sha256 "69d47fc2ba982c42bd5bade4b7c827d5b3ed74b9c59323991c45a9f0f24eb6ed"
 
   license "MIT"
+  revision 1
 
   resource("jfk_fellow_americans") do
     url "https://raw.githubusercontent.com/ggerganov/whisper.cpp/master/samples/jfk.wav"
     sha256 "59dfb9a4acb36fe2a2affc14bacbee2920ff435cb13cc314a08c13f66ba7860e"
+  end
+
+  resource("ggml_base_en") do
+    url "https://huggingface.co/datasets/ggerganov/whisper.cpp/resolve/main/ggml-base.en.bin"
+    sha256 "a03779c86df3323075f5e796cb2ce5029f00ec8869eee3fdfb897afe36c6d002"
   end
 
   def install
@@ -20,7 +26,9 @@ class Whisper < Formula
 
   test do
     resource("jfk_fellow_americans").stage do
-      assert_match "fellow Americans", shell_output("#{bin}/whisper -f jfk.wav")
+      resource("ggml_base_en").stage do
+        assert_match "fellow Americans", shell_output("#{bin}/whisper -f jfk.wav -m ggml-base.en.bin")
+      end
     end
   end
 end
